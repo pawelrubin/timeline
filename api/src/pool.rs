bbdf09f2ee312033435bcf248bbc426d9d2bc794
Paweł Rubin <pawelrubin19@gmail.com>
2021-11-20 21:18:37 +0100

    Simple setup

---
+++ api/src/pool.rs
@@ -0,0 +1,28 @@
+use async_trait::async_trait;
+use sea_orm_rocket::{rocket::figment::Figment, Config, Database};
+
+#[derive(Database, Debug)]
+#[database("timeline")]
+pub struct Db(SeaOrmPool);
+
+#[derive(Debug, Clone)]
+pub struct SeaOrmPool {
+    pub conn: sea_orm::DatabaseConnection,
+}
+
+#[async_trait]
+impl sea_orm_rocket::Pool for SeaOrmPool {
+    type Error = sea_orm::DbErr;
+
+    type Connection = sea_orm::DatabaseConnection;
+
+    async fn init(figment: &Figment) -> Result<Self, Self::Error> {
+        let config = figment.extract::<Config>().unwrap();
+        let conn = sea_orm::Database::connect(&config.url).await?;
+        Ok(SeaOrmPool { conn })
+    }
+
+    fn borrow(&self) -> &Self::Connection {
+        &self.conn
+    }
+}

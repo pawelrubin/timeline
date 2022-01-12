022438ccaad4b26314b5ce970235783f14977ce6
konrad-grochowski <konrad7g@gmail.com>
2022-01-12 01:04:40 +0100

    Full quantize algorithm, not tested, needs memory optimization

--- api/src/quantize.rs
+++ api/src/quantize.rs
@@ -1,77 +1,131 @@
-
-
 use geoutils::Location;
 
-const METERS_THRESHOLD: f64 = 40;
-const MAX_LINE_DEVIATION: f64 = 40; 
-fn geo_distance (p1: (f64,f64), p2: (f64,f64)) -> f64 {
-     
+use crate::auth;
+use crate::db::pool::Db;
+use crate::entities;
+use crate::entities::{GeodataColumn, GeodataEntity, GeodataJson, GeodataModel};
+use itertools::Itertools;
+
+use chrono::{Duration, Utc};
+
+use sea_orm::query::*;
+use sea_orm_rocket::Connection;
+
+use rocket::serde::json::Json;
+use sea_orm::{entity::*, query::*, ConnectionTrait, DatabaseBackend, EntityTrait, Set, Statement};
+
+const METERS_THRESHOLD: f64 = 40.;
+const MAX_LINE_DEVIATION: f64 = 40.;
+const NEW_PATH_TIME_MINUTES: i64 = 60;
+
+fn calculate_dir_vec(v1: [f64;2], v2: [f64;2]) -> [f64;2] {
+    [v2[0] - v1[0], v2[1] - v1[1]]
+}
+
+fn geo_distance(p1: (f64, f64), p2: (f64, f64)) -> f64 {
     let first_location = Location::new(p1.0, p1.1);
     let second_location = Location::new(p2.0, p2.1);
-    let distance_meters = first_location.distance_to(&second_location).unwrap().meters();
+    let distance_meters = first_location
+        .distance_to(&second_location)
+        .unwrap()
+        .meters();
     distance_meters
 }
 
-fn distance_from_line(point: (f64, f64), line_coeffs: (f64, f64)) -> f64 {
-    let (x,y) = point;
-    let (a,b) = line_coeffs;
-    let perpendicular_a = -1/a;
-    let perpendicular_b = y - x * perpendicular_a;
-    let intersect_x = (perpendicular_b - b) / ( a - perpendicular_a)
-    let intersect_y = a * intersect_x + b;
-         
-    geo_distance((x,y), (intersect_x, intersect_y))
+fn filter_geodata_by_distance(geodata_vec: Vec<GeodataModel>) -> Vec<GeodataModel> {
+    let initial_fold_vec: Vec<GeodataModel> = vec![geodata_vec[0].to_owned()];
+
+    // It is worth it to first operate on pointers and then collect values in another iteration,
+    // because tuple_windows() uses clone
+    let filtered = geodata_vec
+        .iter()
+        .tuple_windows()
+        .fold(initial_fold_vec, |mut acc, (prev, next)| {
+            let distance_meters = geo_distance((prev.lat, prev.lng), (next.lat, next.lng));
+            if distance_meters > METERS_THRESHOLD {
+                acc.push(*next);
+            }
+            acc
+        });
+    filtered
 }
 
-
-async fn quantize_geodata
-(
-    conn: Connection<'_, Db>,
-) -> Result<Json<Vec<GeoDataJson>>, rocket::response::Debug<sea_orm::DbErr>> {
-    let db = conn.into_inner();
-    let geodata_vec: Vec<entities::geodata::Model> = Geodata::find()
-        .filter(
-            Condition::all()
-                .add(entities::geodata::Column::Uid.eq(user.sub))
-                .add(
-                    entities::geodata::Column::Timestamp
-                        .gt(Utc::now().naive_utc() - Duration::days(7)),
-                ),
-        )
-        .all(db)
-        .await
-        .expect("could not find geodata");
-
-    let geodata_windows = geodata_vec.into_iter().windows().filter(|prev,next| {
-        let distance_meters = geo_distance((prev.lat, prev.lng), (next.lat, next.lng));
-        distance_meters < METERS_THRESHOLD
-    }).windows();
-    let mut paths = vec![];
-    let mut current_path = vec![filtered_geodata.next(),filtered_geodata.next()];
-    let mut current_line = 
-        
-    //handle only 1 point (here?)
-
-    //CURRENT LINE SETUP HERE
-    let mut line_coeffs: (f64,f64) = get_line_coeffs((prev.lat,prev.lng),(next.lat,next.lng));
-
-    while Some((prev,next)) = geodata_windows.next(){
-
-            let next_is_in_line: bool = MAX_LINE_DEVIATION > distance_from_line((next.lat,next.lng));
-            let next_is_backtracking = //TODO; 
-            if this_is_another_path {
-                paths.push(current_path);
-                current_path = vec![];
+fn split_geodata_by_time<'a>(geodata_vec: Vec<GeodataModel>) -> Vec<Vec<GeodataModel>> {
+    let initial_fold_vec: Vec<Vec<GeodataModel>> = vec![vec![geodata_vec[0].to_owned()]];
+    let split = geodata_vec
+        .iter()
+        .tuple_windows()
+        .fold(initial_fold_vec, |mut acc, (prev, next)| {
+            if next.timestamp - prev.timestamp > Duration::minutes(NEW_PATH_TIME_MINUTES) {
+                //TODO
+                acc.push(vec![]);
             }
-            if !next_is_in_line || this_is_another_path{
-                
-                current_path.push(prev);
-                line_coeffs =  get_line_coeffs((prev.lat,prev.lng),(next.lat,next.lng));
-                //add prev to path as ending of the line
-                // calculate delta next_prev as direction of the new line
-                // 
-                //CURRENT LINE SETUP HERE
-            }
-        }
-    }
+            acc.last_mut().unwrap().push(*next);
+            acc
+        });
+    split
+    
 }
+
+fn vector_length(vec: [f64; 2]) -> f64 {
+    (vec[0]*vec[0]+vec[1]*vec[1]).sqrt()
+}
+
+fn check_if_back_tracking(first: [f64;2], prev: [f64;2], next: [f64;2]) -> bool {
+    let first_to_prev = [first[0] + prev[0], first[1] + prev[1]];
+    let first_to_next =  [first[0] + next[0], first[1] +next[1]];
+    vector_length(first_to_next) < vector_length(first_to_prev)
+
+}
+
+fn calculate_distance_from_line(point: [f64;2],start: [f64;2],end: [f64;2]) -> f64 {
+    let dx = end[0] - start[0];
+    let dy = end[1] - start[1];
+    let s = ((start[1] - point[1]) * dx - (start[0] - point[0]) * dy) /
+        (dx * dx + dy * dy);
+    s.abs() * ((dx * dx + dy * dy)).sqrt()
+}
+
+//TODO last iteration
+fn remove_points_inside_lines(path: Vec<GeodataModel>) -> Vec<GeodataModel> {
+    let mut path_windows = path.iter().tuple_windows();
+    let (first, second) = match path_windows.next() {
+        Some((a, b)) => (a, b),
+        None => return vec![path[0]],
+    };
+    let initial_path_vec = vec![*first];
+    let initial_start = [first.lat, first.lng];
+    let initial_end = [second.lat, second.lng];
+    //TODO check if lat and lng should be swithced
+    let (filtered_path, _, _) = path_windows.fold(
+        (initial_path_vec, initial_start, initial_end),
+        |(mut filtered_path, mut start, mut end), (prev, next)| {
+            let next_xy = [next.lat, next.lng]; 
+            let prev_xy = [prev.lat, prev.lng];
+            let next_is_not_in_line = calculate_distance_from_line(next_xy, start, end) > MAX_LINE_DEVIATION; //TODO the function
+            let next_is_back_tracking = check_if_back_tracking(start, prev_xy, next_xy);
+
+            if next_is_not_in_line || next_is_back_tracking {
+                filtered_path.push(*prev);
+                start = prev_xy;
+                end = next_xy
+            }
+            (filtered_path, start, end)
+        },
+    );
+    filtered_path
+}
+async fn quantize_geodata(
+    geodata_vec: Vec<GeodataModel>
+) -> Vec<Vec<GeodataModel>> {
+    let filtered_geodata: Vec<GeodataModel> = filter_geodata_by_distance(geodata_vec);
+    let paths: Vec<Vec<GeodataModel>> = split_geodata_by_time(filtered_geodata);
+
+    let quantized_paths = paths
+        .into_iter()
+        .map(|path| remove_points_inside_lines(path))
+        .collect::<Vec<Vec<_>>>();
+
+    quantized_paths
+}
+
